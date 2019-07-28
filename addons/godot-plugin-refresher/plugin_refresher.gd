f42cb6aea2633e5f9f1013f5eeb93239339270cf
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2019-07-28 19:45:14 +0300

    Save and reselect recently refreshed plugin
    This allows developers to stay on the recently reloaded plugin
    during and between editor sessions by making use of plugin
    custom configuration file which is saved at project settings directory.
    
    Also updated project settings to match with Godot 3.1-stable.
    Also works with Godot 3.2-dev.
    
    Disabled non-existing plugin which was used for testing apparently.

--- addons/godot-plugin-refresher/plugin_refresher.gd
+++ addons/godot-plugin-refresher/plugin_refresher.gd
@@ -1,6 +1,9 @@
 tool
 extends HBoxContainer
 
+const ADDONS_PATH = "res://addons/"
+const PLUGIN_PATH = "godot-plugin-refresher"
+
 signal request_refresh_plugin(p_name)
 
 onready var options = $OptionButton
@@ -12,17 +15,31 @@ func reload_items():
 	if not options:
 		return
 	var dir = Directory.new()
-	dir.change_dir("res://addons/")
+	dir.change_dir(ADDONS_PATH)
 	dir.list_dir_begin(true, true)
 	var file = dir.get_next()
 	options.clear()
 	while file:
-		if dir.dir_exists("res://addons/" + file) and file != "godot-plugin-refresher":
+		if dir.dir_exists(ADDONS_PATH.plus_file(file)) and file != PLUGIN_PATH:
 			options.add_item(file)
 		file = dir.get_next()
 
+func select_plugin(p_name):
+	if not options:
+		return
+	if p_name == null or p_name.empty():
+		return
+
+	for idx in options.get_item_count():
+		var plugin = options.get_item_text(idx)
+		if plugin == p_name:
+			options.selected = options.get_item_id(idx)
+
 func _on_RefreshButton_pressed():
+	if options.selected == -1:
+		return # nothing selected
+
 	var plugin = options.get_item_text(options.selected)
-	if not plugin:
+	if not plugin or plugin.empty():
 		return
 	emit_signal("request_refresh_plugin", plugin)
