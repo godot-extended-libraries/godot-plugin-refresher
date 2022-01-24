b03b81339231685938fa14c624697e0ec75c4dc0
Mounir Tohami <whalesstate@gmail.com>
2022-01-24 09:08:35 +0200

    fix
    removed "_" and changed variable names to more detailed names

--- addons/godot-plugin-refresher/plugin_refresher.gd
+++ addons/godot-plugin-refresher/plugin_refresher.gd
@@ -1,84 +1,83 @@
 tool
 extends HBoxContainer
 
-signal request_refresh_plugin(p_name)
-signal request_enable_plugin(p_name)
+signal request_refresh_plugin(plugin_name)
+signal request_enable_plugin(plugin_name)
 #warning-ignore:unused_signal
-signal request_disable_plugin(p_name)
-signal plugin_changed(p_name)
+signal request_disable_plugin(plugin_name)
+signal plugin_changed(plugin_name)
 
 var cur_plugin := ""
-var enabled := false
+var is_enabled := false
 
 onready var checkbox: CheckBox = $CheckBox
-onready var options: OptionButton = $OptionButton
-onready var refresh: Button = $RefreshButton
-onready var confirm: ConfirmationDialog = $ConfirmationDialog
+onready var options_button: OptionButton = $OptionButton
+onready var refresh_button: Button = $RefreshButton
+onready var confirmation_dialog: ConfirmationDialog = $ConfirmationDialog
 
 
 func _ready() -> void:
-	# var _cur_scene = get_tree().edited_scene_root
-	# assert(_cur_scene != self, "Scene: '%s' is opened in editor!" % _cur_scene)
 	assert(OK == checkbox.connect("toggled", self, "_on_checkbox_toggled"))
-	assert(OK == options.connect("item_selected", self, "_on_options_item_selected"))
-	assert(OK == refresh.connect("pressed", self, "_on_refresh_button_pressed"))
-	assert(OK == confirm.connect("confirmed", self, "_on_conf_dialog_confirmed"))
-	refresh.icon = get_icon("Reload", "EditorIcons")
+	assert(OK == options_button.connect("item_selected", self, "_on_options_item_selected"))
+	assert(OK == refresh_button.connect("pressed", self, "_on_refresh_button_pressed"))
+	assert(OK == confirmation_dialog.connect("confirmed", self, "_on_conf_dialog_confirmed"))
+	refresh_button.icon = get_icon("Reload", "EditorIcons")
 
-func update_items(_plugins: Dictionary) -> void:
-	if not options:
+
+func update_items(plugins: Dictionary) -> void:
+	if not options_button:
 		return
-	options.clear()
-	var _dirs: Array = _plugins.keys()
-	for i in _dirs.size():
-		var _dirname: String = _dirs[i]
-		options.add_item(_plugins[_dirname], i)
-		options.set_item_metadata(i, _dirname)
+	options_button.clear()
+	var dirs: Array = plugins.keys()
+	for i in dirs.size():
+		var dir_name: String = dirs[i]
+		options_button.add_item(plugins[dir_name], i)
+		options_button.set_item_metadata(i, dir_name)
 	if cur_plugin.empty():
-		if not options.get_item_count():
+		if not options_button.get_item_count():
 			return
-		cur_plugin = options.get_item_metadata(0)
+		cur_plugin = options_button.get_item_metadata(0)
 		emit_signal("plugin_changed", cur_plugin)
-		checkbox.pressed = enabled
+		checkbox.pressed = is_enabled
 
 
-func select_plugin(p_name: String) -> void:
-	if not options or p_name.empty():
+func select_plugin(plugin_name: String) -> void:
+	if not options_button or plugin_name.empty():
 		return
-	for i in options.get_item_count():
-		cur_plugin = options.get_item_metadata(i)
-		if cur_plugin == p_name:
-			options.selected = options.get_item_id(i)
+	for i in options_button.get_item_count():
+		cur_plugin = options_button.get_item_metadata(i)
+		if cur_plugin == plugin_name:
+			options_button.selected = options_button.get_item_id(i)
 			emit_signal("plugin_changed", cur_plugin)
-			checkbox.pressed = enabled
+			checkbox.pressed = is_enabled
 			break
 
 
-func show_warning(_p_name: String) -> void:
-	var _text: String = "Plugin '%s' is disabled.\n Do you want to enable it?" % _p_name
-	confirm.dialog_text = _text
-	confirm.popup_centered()
+func show_warning(plugin_name: String) -> void:
+	var warning_message: String = "Plugin '%s' is disabled.\n Do you want to enable it?" % plugin_name
+	confirmation_dialog.dialog_text = warning_message
+	confirmation_dialog.popup_centered()
 
 
-func _on_options_item_selected(_ind: int) -> void:
-	cur_plugin = options.get_item_metadata(options.selected)
+func _on_options_item_selected(id: int) -> void:
+	cur_plugin = options_button.get_item_metadata(options_button.selected)
 	emit_signal("plugin_changed", cur_plugin)
-	checkbox.pressed = enabled
+	checkbox.pressed = is_enabled
 
 
-func _on_checkbox_toggled(_pressed: bool) -> void:
-	if not options.get_item_count():
+func _on_checkbox_toggled(pressed: bool) -> void:
+	if not options_button.get_item_count():
 		checkbox.pressed = false
 		return
-	emit_signal("request_%s_plugin" % ("enable" if _pressed else "disable"), cur_plugin)
+	emit_signal("request_%s_plugin" % ("enable" if pressed else "disable"), cur_plugin)
 
 
 func _on_refresh_button_pressed() -> void:
-	if not options.get_item_count():
+	if not options_button.get_item_count():
 		return
 	emit_signal("request_refresh_plugin", cur_plugin)
 
 
 func _on_conf_dialog_confirmed() -> void:
 	emit_signal("request_enable_plugin", cur_plugin)
-	checkbox.pressed = enabled
+	checkbox.pressed = is_enabled
--- addons/godot-plugin-refresher/plugin_refresher_plugin.gd
+++ addons/godot-plugin-refresher/plugin_refresher_plugin.gd
@@ -3,21 +3,21 @@ extends EditorPlugin
 
 const Refresher = "./plugin_refresher.tscn"
 
-var cfg: ConfigFile = ConfigFile.new()
+var cfg := ConfigFile.new()
 var refresher: HBoxContainer
 
 
 func _enter_tree() -> void:
 	refresher = preload(Refresher).instance()
 	add_control_to_container(0, refresher)
-	var _fs := get_editor_interface().get_resource_filesystem()
-	assert(OK == _fs.connect("filesystem_changed", self, "_on_filesystem_changed"))
+	var filesystem := get_editor_interface().get_resource_filesystem()
+	assert(OK == filesystem.connect("filesystem_changed", self, "_on_filesystem_changed"))
 	assert(OK == refresher.connect("request_refresh_plugin", self, "_on_refresh_plugin_request"))
 	assert(OK == refresher.connect("request_enable_plugin", self, "_on_enable_request", [true]))
 	assert(OK == refresher.connect("request_disable_plugin", self, "_on_enable_request", [false]))
 	assert(OK == refresher.connect("plugin_changed", self, "_on_plugin_changed"))
-	reload_plugins_list()
-	load_settings()
+	_reload_plugins_list()
+	_load_settings()
 
 
 func _exit_tree() -> void:
@@ -25,99 +25,99 @@ func _exit_tree() -> void:
 	refresher.queue_free()
 
 
-func reload_plugins_list() -> void:
-	var _dir := Directory.new()
-	assert(OK == _dir.open("res://addons/"))
-	assert(OK == _dir.list_dir_begin(true, true))
-	var _file := _dir.get_next()
-	var _plugins := {}
-	var _origins := {}
-	while _file:
-		var _addon_dir := "res://addons/%s" % _file
-		var _is_cur_plugin := (_file == get_plugin_path().get_file())
-		if _dir.dir_exists(_addon_dir) and not _is_cur_plugin:
-			var _config_path = "%s/plugin.cfg" % _addon_dir
-			if not _dir.file_exists(_config_path):
-				_file = _dir.get_next()
+func _reload_plugins_list() -> void:
+	var dir := Directory.new()
+	assert(OK == dir.open("res://addons/"))
+	assert(OK == dir.list_dir_begin(true, true))
+	var display_name := dir.get_next()
+	var plugins := {}
+	var origins := {}
+	while display_name:
+		var addon_dir := "res://addons/%s" % display_name
+		var is_cur_plugin := (display_name == _get_plugin_path().get_file())
+		if dir.dir_exists(addon_dir) and not is_cur_plugin:
+			var config_path = "%s/plugin.cfg" % addon_dir
+			if not dir.file_exists(config_path):
+				display_name = dir.get_next()
 				continue
-			var _plugin_cfg := ConfigFile.new()
-			assert(OK == _plugin_cfg.load(_config_path))
-			var _p_name: String = _plugin_cfg.get_value("plugin", "name", _file)
-			if not _p_name in _origins:
-				_origins[_p_name] = [_file]
+			var plugin_cfg := ConfigFile.new()
+			assert(OK == plugin_cfg.load(config_path))
+			var plugin_name: String = plugin_cfg.get_value("plugin", "name", display_name)
+			if not plugin_name in origins:
+				origins[plugin_name] = [display_name]
 			else:
-				_origins[_p_name].append(_file)
-				_p_name = "%s (%s)" % [_p_name, _file]
-			_plugins[_file] = _p_name
-		_file = _dir.get_next()
-	refresher.update_items(_plugins)
+				origins[plugin_name].append(display_name)
+				plugin_name = "%s (%s)" % [plugin_name, display_name]
+			plugins[display_name] = plugin_name
+		display_name = dir.get_next()
+	refresher.update_items(plugins)
 
 
-func get_plugin_path() -> String:
+func _get_plugin_path() -> String:
 	return get_script().resource_path.get_base_dir()
 
 
-func load_settings()-> void:
-	var _cfg_path := get_config_path()
-	var _dir := Directory.new()
-	if not _dir.file_exists(_cfg_path):
+func _load_settings()-> void:
+	var cfg_path := _get_config_path()
+	var dir := Directory.new()
+	if not dir.file_exists(cfg_path):
 		var config := ConfigFile.new()
-		assert(OK == _dir.make_dir_recursive(_cfg_path.get_base_dir()))
-		assert(OK == config.save(_cfg_path))
+		assert(OK == dir.make_dir_recursive(cfg_path.get_base_dir()))
+		assert(OK == config.save(cfg_path))
 	else:
-		cfg.load(_cfg_path)
+		cfg.load(cfg_path)
 
 
-func get_config_path() -> String:
-	var _dir := get_editor_interface().get_editor_settings().get_project_settings_dir()
-	return "%s/plugins/plugin_refresher/settings.cfg" % _dir
+func _get_config_path() -> String:
+	var dir := get_editor_interface().get_editor_settings().get_project_settings_dir()
+	return "%s/plugins/plugin_refresher/settings.cfg" % dir
 
 
-func save_settings() -> void:
-	cfg.save(get_config_path())
+func _save_settings() -> void:
+	assert(OK == cfg.save(_get_config_path()))
 
 
 func _on_filesystem_changed() -> void:
 	if not refresher:
 		return
-	reload_plugins_list()
-	refresher.select_plugin(get_recent_plugin())
+	_reload_plugins_list()
+	refresher.select_plugin(_get_recent_plugin())
 
 
-func get_recent_plugin() -> String:
+func _get_recent_plugin() -> String:
 	return cfg.get_value("settings", "recently_used", "")
 
 
-func is_enabled(_p_name: String) -> bool:
-	return get_editor_interface().is_plugin_enabled(_p_name)
+func is_enabled(plugin_name: String) -> bool:
+	return get_editor_interface().is_plugin_enabled(plugin_name)
 
 
-func _on_enable_request(_p_name: String, _enable: bool) -> void:
-	if not _p_name or is_enabled(_p_name) == _enable:
+func _on_enable_request(plugin_name: String, enable: bool) -> void:
+	if not plugin_name or is_enabled(plugin_name) == enable:
 		return
-	get_editor_interface().set_plugin_enabled(_p_name, _enable)
-	refresher.enabled = is_enabled(_p_name)
-	cfg.set_value("settings", "recently_used", _p_name)
-	save_settings()
+	get_editor_interface().set_plugin_enabled(plugin_name, enable)
+	refresher.enabled = is_enabled(plugin_name)
+	cfg.set_value("settings", "recently_used", plugin_name)
+	_save_settings()
 
 
-func _on_refresh_plugin_request(_p_name: String) -> void:
-	if not _p_name:
+func _on_refresh_plugin_request(plugin_name: String) -> void:
+	if not plugin_name:
 		return
-	if not is_enabled(_p_name):
-		refresher.show_warning(_p_name)
+	if not is_enabled(plugin_name):
+		refresher.show_warning(plugin_name)
 	else:
-		refresher.emit_signal("request_disable_plugin", _p_name)
-		refresher.call_deferred("emit_signal", "request_enable_plugin", _p_name)
+		refresher.emit_signal("request_disable_plugin", plugin_name)
+		refresher.call_deferred("emit_signal", "request_enable_plugin", plugin_name)
 
 
-func _on_confirm_refresh_plugin(_p_name: String) -> void:
-	refresher.emit_signal("request_enable_plugin", _p_name)
+func _on_confirm_refresh_plugin(plugin_name: String) -> void:
+	refresher.emit_signal("request_enable_plugin", plugin_name)
 
 
-func _on_plugin_changed(_p_name: String) -> void:
+func _on_plugin_changed(plugin_name: String) -> void:
 	if not refresher:
 		return
-	refresher.enabled = is_enabled(_p_name)
-	cfg.set_value("settings", "recently_used", _p_name)
-	save_settings()
+	refresher.enabled = is_enabled(plugin_name)
+	cfg.set_value("settings", "recently_used", plugin_name)
+	_save_settings()
