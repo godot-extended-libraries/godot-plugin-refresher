55a49cb0307694deff3c1ed5ace3e3b923ce8585
willnationsdev <willnationsdev@gmail.com>
2024-12-08 01:51:43 -0600

    Fix Edge cases where config files >1 dir deep.
    - Adapted from PR #24.
    - Credit to ACB-prgm for the initial implementation.

--- addons/godot-plugin-refresher/plugin_refresher.gd
+++ addons/godot-plugin-refresher/plugin_refresher.gd
@@ -9,21 +9,29 @@ signal confirm_refresh_plugin(p_name: String)
 
 func _ready() -> void:
 	if get_tree().edited_scene_root == self:
-		return  # This is the scene opened in the editor!
+		return # This is the scene opened in the editor!
 	$RefreshButton.icon = EditorInterface.get_editor_theme().get_icon("Reload", "EditorIcons")
 
 
-func update_items(p_plugins: Dictionary) -> void:
+func update_items(p_plugins_info: Array) -> void:
 	if not options:
 		return
 	options.clear()
+
+	var plugins := p_plugins_info[0] as Dictionary
+	var display_names_map := p_plugins_info[1] as Dictionary
+
 	var plugin_dirs: Array[String] = []
-	plugin_dirs.assign(p_plugins.keys())
+	plugin_dirs.assign(plugins.keys())
 	for idx in plugin_dirs.size():
-		var plugin_dirname := plugin_dirs[idx] as String
-		var plugin_name := p_plugins[plugin_dirname] as String
-		options.add_item(plugin_name, idx)
-		options.set_item_metadata(idx, plugin_dirname)
+		var plugin_dirname := plugin_dirs[idx]
+		var plugin_data = plugins[plugin_dirname] # Array[String] used as a Tuple<String, String>.
+		var plugin_name := plugin_data[0] as String
+		var plugin_path := plugin_data[1] as String
+		var display_name := display_names_map[plugin_path] as String
+
+		options.add_item(display_name, idx)
+		options.set_item_metadata(idx, plugin_path)
 
 
 # Note: For whatever reason, statically typing `p_name` inexplicably causes
@@ -41,7 +49,7 @@ func select_plugin(p_name) -> void:
 
 func _on_RefreshButton_pressed() -> void:
 	if options.selected == -1:
-		return  # nothing selected
+		return # nothing selected
 
 	var plugin := str(options.get_item_metadata(options.selected))
 	if not plugin:
